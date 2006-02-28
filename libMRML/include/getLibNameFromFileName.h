98dc9d58e6dd919fef6016b8656133ab04631f41
Wolfgang Mueller <Wolfgang.Mueller@cui.unige.ch>
2006-02-28 14:52:10 +0000

    for ernesto

---
+++ libMRML/include/getLibNameFromFileName.h
@@ -0,0 +1,32 @@
+/* -*- mode: c++ -*- 
+*/
+/* 
+
+    GIFT, a flexible content based image retrieval system.
+    Copyright (C) 1998, 1999, 2000, 2001, 2002, CUI University of Geneva
+
+     Copyright (C) 2003, 2004 Bayreuth University
+    Copyright (C) 2005,2006 Bamberg University
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+// -*- mode: c++ -*-
+#ifndef _GETLIBNAMEFROMFILENAME
+#define _GETLIBNAMEFROMFILENAME
+//#include <map>
+#include <string>
+
+extern std::pair<bool,std::string> getLibNameFromFileName(std::string inPrefix,std::string inName);
+#endif
